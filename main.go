a0776be4bef999be5bd9f2742b7cdbbb5654357f
Adrian Cuellar <luis.cuellar@alumnos.udg.mx>
2020-09-29 21:05:27 -0500

    prueba 1 numero e

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var limite_ciclo float64
+	var n float64 = 1.0
+	var resultado float64 = 0
+	var e float64
+	fmt.Scanln(&limite_ciclo)
+	for n <= limite_ciclo {
+		e = 1.0 / n
+		resultado = resultado + e
+		n++
+	}
+	fmt.Println(resultado)
 }
